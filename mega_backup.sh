23751969293fd15f99efd2174ff865b986dcb683
DenisVS <denvsim@gmail.com>
2024-01-26 15:18:47 +0100

    copy new dirs without dates

--- mega_backup.sh
+++ mega_backup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# Only nested directories in configured paths will be copied correctly!
+# MEGA_BACKUP_PATHS file
+# c:/Users/master/Downloads/test dir|1    copy only missing directories
+# #c:/Shares|0                            commented line, skip
+# c:/Users/master/Documents/Projects|0    copy every directories into new DATE-TIME directory
+
 MEGATOOLS_PATH='/cygdrive/c/PF/megatools-1.11.1.20230212-win64/megatools.exe'
 MEGA_CREDENTIALS_PATH='/cygdrive/c/PF/megatools-1.11.1.20230212-win64/mega.ini'
 MEGA_BACKUP_PATHS='/cygdrive/c/PF/megatools-1.11.1.20230212-win64/mega_server_backup_paths.ini'
@@ -10,7 +16,7 @@ PASSWORD=$(cat "$MEGA_CREDENTIALS_PATH" | grep Password | awk -F \= '{print $2}'
 
 ### analog of mkdir -p
 PATH_BEFORE_ACTUAL_DIR=""
-$MEGATOOLS_PATH test -d  --reload "/Root" ## Refresh cache
+$MEGATOOLS_PATH test -d  --reload "/Root"        ## Refresh cache
 PATH_ARRAY=$(echo $MEGA_CLOUD_PATH | tr "/" " ") #MEGA path splitted
 for CURRENT_LAST_DIR_IN_CYCLE in ${PATH_ARRAY}; do
   PATH_BEFORE_ACTUAL_DIR="$PATH_BEFORE_ACTUAL_DIR/$CURRENT_LAST_DIR_IN_CYCLE"
@@ -36,6 +42,47 @@ echo 0 $PATHS_0
 echo 1 $PATHS_1
 #exit
 
+#################################################
+## New block of direct copy dirs which don't exist
+SAVEIFS=${IFS}
+IFS='
+'
+for CURRENT_PATH in ${PATHS_1}; do
+  echo Current Path $CURRENT_PATH
+  LAST_DIR=$(echo $CURRENT_PATH | awk -F "/" '{print $NF}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+  echo Last dir: $LAST_DIR
+  echo Mega cloud path: $MEGA_CLOUD_PATH
+
+  #create dir for not by date backups
+  $MEGATOOLS_PATH test -d "${MEGA_CLOUD_PATH}/${LAST_DIR}"
+  TEST="$?"
+  echo test $TEST
+  if [ "$TEST" = 0 -o $PATH_BEFORE_ACTUAL_DIR = '/Root' ]; then
+    echo Dir exists
+  else
+    echo Let\'s make "${MEGA_CLOUD_PATH}/${LAST_DIR}"
+    $MEGATOOLS_PATH mkdir "${MEGA_CLOUD_PATH}/${LAST_DIR}"
+  fi
+
+  LOCAL_DIRS_TO_BACKUP=$(find $CURRENT_PATH -maxdepth 1 -mindepth 1 -type d | awk -F \/ '{print $NF}')
+  ### Cycle all dirs in folders
+  for CURRENT_DIR_TO_BACKUP in ${LOCAL_DIRS_TO_BACKUP}; do
+    $MEGATOOLS_PATH test -d "${MEGA_CLOUD_PATH}/${LAST_DIR}/${CURRENT_DIR_TO_BACKUP}"
+    TEST="$?"
+    echo test $TEST
+    if [ "$TEST" = 0 ]; then
+      echo Dir exists
+    else
+      echo Let\'s copy "${MEGA_CLOUD_PATH}/${LAST_DIR}/${CURRENT_DIR_TO_BACKUP}"
+      $MEGATOOLS_PATH copy --local "${CURRENT_PATH}/${CURRENT_DIR_TO_BACKUP}" --remote "${MEGA_CLOUD_PATH}/${LAST_DIR}"
+    fi
+
+  done
+done
+#exit
+
+#################################################
+# Create dirs and copy there content by date.
 for CURRENT_PATH in ${PATHS_0}; do
   echo $CURRENT_PATH
   LAST_DIR=$(echo $CURRENT_PATH | awk -F "/" '{print $NF}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
@@ -45,23 +92,6 @@ for CURRENT_PATH in ${PATHS_0}; do
   $MEGATOOLS_PATH copy --local "${CURRENT_PATH}" --remote "$PATH_BEFORE_ACTUAL_DIR/$DATE/$LAST_DIR"
 done
 
-#################################################
-## New block of direct copy dirs which don't exist
-exit
-for CURRENT_PATH in ${PATHS_1}; do
-  echo $CURRENT_PATH
-  LAST_DIR=$(echo $CURRENT_PATH | awk -F "/" '{print $NF}' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-  echo Last dir: $LAST_DIR
-  #$MEGATOOLS_PATH mkdir "$PATH_BEFORE_ACTUAL_DIR/$LAST_DIR"
-
-  ### Cycle all dirs in folders
-
-  #CURRENT_PATH=$(echo $CURRENT_PATH | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-  echo Current Path "$CURRENT_PATH"
-  #$MEGATOOLS_PATH copy --local "${CURRENT_PATH}" --remote "$PATH_BEFORE_ACTUAL_DIR/$DATE/$LAST_DIR"
-
-done
-exit
 #################################################
 
 IFS=${SAVEIFS}

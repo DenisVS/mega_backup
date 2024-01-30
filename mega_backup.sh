decfab2fe47b84b673ac39ab29cb38e3543063dc
DenisVS <denvsim@gmail.com>
2024-01-30 14:34:55 +0100

    without path declares

--- mega_backup.sh
+++ mega_backup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-# Only nested directories in configured paths will be copied correctly!
+# Only nested downstream  directories in configured paths will be copied correctly!
 # MEGA_BACKUP_PATHS file
-# c:/Users/master/Downloads/test dir|1    copy only missing directories
-# #c:/Shares|0                            commented line, skip
-# c:/Users/master/Documents/Projects|0    copy every directories into new DATE-TIME directory
+# c:/Users/master/Downloads/test dir|/Root/Backup38/AA|1    copy only missing directories
+# #c:/Shares|/Root/Backup38|0                            commented line, skip
+# c:/Users/master/Documents/Projects|/Root/Backup2|0    copy every directories into new DATE-TIME directory
+#PATH=$(echo /usr/local/bin:/usr/bin)
+
 . ./config.sh
 . ./functions.sh
 
@@ -18,7 +20,6 @@ for CURRENT_ELEMENT in ${BACKUP_OBJECT_0}; do
   echo ELEMENT_CLOUD_PATH $ELEMENT_CLOUD_PATH
   echo sent "$ELEMENT_CLOUD_PATH" "-----> function mkdir"
   _mega_mkdir_p "$MEGATOOLS_PATH" "$USERNAME" "$PASSWORD" "$ELEMENT_CLOUD_PATH"
-  PATH=$(echo /usr/local/bin:/usr/bin)
   find "$ELEMENT_LOCAL_PATH" -maxdepth 1 -mindepth 1 -type d | awk -F \/ '{print $NF}'
   LOCAL_DIRS_TO_BACKUP=$(find "$ELEMENT_LOCAL_PATH" -maxdepth 1 -mindepth 1 -type d | awk -F \/ '{print $NF}')
   echo LOCAL_DIRS_TO_BACKUP $LOCAL_DIRS_TO_BACKUP
@@ -31,7 +32,6 @@ for CURRENT_ELEMENT in ${BACKUP_OBJECT_0}; do
     echo ELEMENT_CLOUD_PATH/CURRENT_DIR_TO_BACKUP:
     echo sent "$ELEMENT_CLOUD_PATH/$CURRENT_DIR_TO_BACKUP" "$ELEMENT_LOCAL_PATH-----> function copy"
     _mega_copy_if_not_exist "$MEGATOOLS_PATH" "$USERNAME" "$PASSWORD" "$ELEMENT_CLOUD_PATH/$CURRENT_DIR_TO_BACKUP" "$ELEMENT_LOCAL_PATH"
-    PATH=$(echo /usr/local/bin:/usr/bin)
   done
   IFS=${SAVEIFS}
   echo -------------------------
@@ -47,10 +47,8 @@ for CURRENT_ELEMENT in ${BACKUP_OBJECT_1}; do
   echo ELEMENT_LOCAL_PATH $ELEMENT_LOCAL_PATH
   echo ELEMENT_CLOUD_PATH $ELEMENT_CLOUD_PATH
   _mega_mkdir_p "$MEGATOOLS_PATH" "$USERNAME" "$PASSWORD" "$ELEMENT_CLOUD_PATH/$DATE"
-  PATH=$(echo /usr/local/bin:/usr/bin)
   echo sent "$ELEMENT_CLOUD_PATH" "$ELEMENT_LOCAL_PATH--"$DATE"---> function copy"
   _mega_copy_if_not_exist "$MEGATOOLS_PATH" "$USERNAME" "$PASSWORD" "$ELEMENT_CLOUD_PATH" "$ELEMENT_LOCAL_PATH" "$DATE"
-  PATH=$(echo /usr/local/bin:/usr/bin)
 
   echo -------------------------
 done
